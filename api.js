e063b6e6ee01a255fb257ebbdefda5df5a909e63
jaydipraithatha10 <jaydipraithatha10@gmail.com>
2026-07-25 22:00:38 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,40 @@
 
+// ==============================
+// Google Sheet CSV URLs
+// ==============================
+
+const categoryURL =
+"https://docs.google.com/spreadsheets/d/e/2PACX-1vStfoYZJzDES0lAav3gzVi4hHMrr-g-vu6oHbAecwVN7-j5ZfyZCE4wy5qE8oaH0fSw14Y97pHMmUrU/pub?gid=2013716827&single=true&output=csv";
+
+const subCategoryURL =
+"https://docs.google.com/spreadsheets/d/e/2PACX-1vStfoYZJzDES0lAav3gzVi4hHMrr-g-vu6oHbAecwVN7-j5ZfyZCE4wy5qE8oaH0fSw14Y97pHMmUrU/pub?gid=35788410&single=true&output=csv";
+
+const productURL =
+"https://docs.google.com/spreadsheets/d/e/2PACX-1vStfoYZJzDES0lAav3gzVi4hHMrr-g-vu6oHbAecwVN7-j5ZfyZCE4wy5qE8oaH0fSw14Y97pHMmUrU/pub?gid=0&single=true&output=csv";
+
+
+// ==============================
+// Read CSV
+// ==============================
+
+async function fetchCSV(url) {
+
+    const response = await fetch(url);
+    const text = await response.text();
+
+    return text;
+
+}
+
+
+// ==============================
+// CSV to Array
+// ==============================
+
+function csvToArray(csv) {
+
+    const rows = csv.trim().split("\n");
+
+    return rows.map(row => row.split(","));
+
+}
