a3549acbc98caf67980d8108a2abe997e8e15e0a
jaydipraithatha10 <jaydipraithatha10@gmail.com>
2026-07-25 20:54:46 +0530

    Update include.js

--- include.js
+++ include.js
@@ -1,1 +1,30 @@
 
+// Load Header
+fetch("header.html")
+.then(response => response.text())
+.then(data => {
+
+    const header = document.getElementById("header");
+
+    if(header){
+
+        header.innerHTML = data;
+
+    }
+
+});
+
+// Load Footer
+fetch("footer.html")
+.then(response => response.text())
+.then(data => {
+
+    const footer = document.getElementById("footer");
+
+    if(footer){
+
+        footer.innerHTML = data;
+
+    }
+
+});
